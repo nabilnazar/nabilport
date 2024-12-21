93f0bd3347c811c2b25beaf2a3819fd8e7420ba8
Nabil Nazar <nabilnazarp@gmail.com>
2024-12-22 02:39:03 +0530

    pending fix periodic update of db when connected to n/w

---
+++ offlinefirstapp/app/src/main/java/com/nabilnazar/offlinefirstapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.nabilnazar.offlinefirstapp
+
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.os.Bundle
+ import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.nabilnazar.offlinefirstapp.presentation.ui.MainScreen
+import com.nabilnazar.offlinefirstapp.ui.theme.OfflinefirstappTheme
+import com.nabilnazar.offlinefirstapp.workmanager.NetworkChangeReceiver
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+        setContent {
+            OfflinefirstappTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainScreen()
+                }
+            }
+        }
+    }
+
+
+}
+
